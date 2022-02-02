647f3a0e4c2563b3bb15c2cb103c1f7a671bb5e6
yflying7@gamil.com <yflying7>
2022-02-02 19:21:29 +0800

    logging

--- src/log.py
+++ src/log.py
@@ -2,7 +2,8 @@
 import logging
 
 logger = logging.getLogger("Spider")
-handler = logging.StreamHandler()
+# handler = logging.StreamHandler()
+handler = logging.FileHandler('xvideo.log')
 formatter = logging.Formatter('[%(asctime)s][%(name)-4s][%(levelname)-4s] > %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
