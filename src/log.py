1b99ae9eb8d40167b850d16e7b8bc2a7079c5de5
yflying7@gamil.com <yflying7>
2022-02-02 19:41:53 +0800

    fh_handler = logging.FileHandler('logs/xvideo.log')

--- src/log.py
+++ src/log.py
@@ -1,10 +1,18 @@
 # -*- coding:utf-8 -*-
+import os
 import logging
 
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+
 logger = logging.getLogger("Spider")
-# handler = logging.StreamHandler()
-handler = logging.FileHandler('xvideo.log')
+ch_handler = logging.StreamHandler()
+fh_handler = logging.FileHandler('logs/xvideo.log')
+
 formatter = logging.Formatter('[%(asctime)s][%(name)-4s][%(levelname)-4s] > %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
+ch_handler.setFormatter(formatter)
+fh_handler.setFormatter(formatter)
+
+logger.addHandler(ch_handler)
+logger.addHandler(fh_handler)
 logger.setLevel(logging.INFO)
